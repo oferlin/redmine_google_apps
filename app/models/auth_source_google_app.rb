b0c587d25b2d834e8e52689a27729d910ee34230
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-07-20 23:59:46 +0700

    Test connection functionality added

--- app/models/auth_source_google_app.rb
+++ app/models/auth_source_google_app.rb
@@ -4,7 +4,11 @@ class AuthSourceGoogleApp < AuthSource
   validates :host, presence: true, length: { maximum: 60 }
 
   def test_connection
-    true
+    @discovery = OpenID::GoogleDiscovery.new
+    info = @discovery.perform_discovery(host)
+    unless info
+      raise AuthSourceException.new(I18n.t(:error_google_apps_discovery_info_not_found, host: host))
+    end
   end
 
   def auth_method_name
