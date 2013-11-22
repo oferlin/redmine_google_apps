be6b68bd59175a0f9dc45b433006da0e255f8510
mikrob <mrobert@octo.com>
2013-11-22 13:26:08 +0100

    Make migration of issue if user was existing before openid

--- app/controllers/google_apps_controller.rb
+++ app/controllers/google_apps_controller.rb
@@ -14,18 +14,21 @@ class GoogleAppsController < AccountController
 
   protected
 
+  UGGLY_HASH = {
+    
+  }
+
   def authenticate(domain)
     attributes = [AX_EMAIL, AX_FIRST, AX_LAST]
     oid = "https://www.google.com/accounts/o8/site-xrds?hd=#{domain.host}"
 
     authenticate_with_open_id(oid, :return_to => request.url, :required => attributes) do |result, identity_url|
+      old_user_id, new_user_id = nil
       if result.successful?
         user = User.find_or_initialize_by_identity_url(identity_url)
-
         if user.new_record?
           ax_response = OpenID::AX::FetchResponse.from_success_response(request.env[Rack::OpenID::RESPONSE])
           email, first, last = [ax_response[AX_EMAIL], ax_response[AX_FIRST], ax_response[AX_LAST]].map(&:first)
-
           user.login       = email[/^[^@]*/] unless email.nil?
           user.mail        = email
           user.firstname   = first
@@ -33,6 +36,12 @@ class GoogleAppsController < AccountController
           user.auth_source = domain
           user.random_password
           user.register
+
+          old_user_trigram = UGGLY_HASH[email]
+          user_old = User.where(:mail => "#{old_user_trigram}@octo.com")
+          logger.info "User old : #{user_old.inspect}"
+          old_user_id = user_old.first.id if user_old
+          logger.info "Old User Id : #{old_user_id}"
 
           if domain.onthefly_register?
             register_automatically(user) do
@@ -55,6 +64,14 @@ class GoogleAppsController < AccountController
         logger.warn "Failed login with domain '#{domain.host}' from #{request.remote_ip} at #{Time.now.utc}"
         flash[:error] = result.message
         return redirect_to :controller => :account, :action => :login
+      end
+      if old_user_id
+        new_user_id = user.id
+        logger.info "New user id : #{user.inspect}"
+        logger.info "New user id : #{new_user_id}: #{user.inspect}"
+        Issue.where(:author_id => old_user_id).map { |issue| issue.author_id = new_user_id; issue.save! }
+        Issue.where(:assigned_to_id => old_user_id).map { |issue| issue.assigned_to_id = new_user_id; issue.save! }
+        User.destroy(old_user_id)
       end
     end
   end
