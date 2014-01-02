d2f81eb26c251beccbf763e2518127dcd8dd1b99
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-02 17:42:34 +0100

    Fixing the routes.rb

--- app/models/auth_source_google_app.rb
+++ app/models/auth_source_google_app.rb
@@ -1,13 +1,13 @@
 class AuthSourceGoogleApp < AuthSource
   unloadable
 
-  validates :host, presence: true, length: { maximum: 60 }
+  validates :host, :presence => true, :length => { :maximum => 60 }
 
   def test_connection
     @discovery = OpenID::GoogleDiscovery.new
     info = @discovery.perform_discovery(host)
     unless info
-      raise AuthSourceException.new(I18n.t(:error_google_apps_discovery_info_not_found, host: host))
+      raise AuthSourceException.new(I18n.t(:error_google_apps_discovery_info_not_found, :host => host))
     end
   end
 
